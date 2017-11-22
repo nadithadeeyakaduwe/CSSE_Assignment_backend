e4860fe54fcd393743a1b4a4b19804d67675a452
PWithanage <psathsaranee@gmail.com>
2017-11-22 22:47:14 +0530

    edit supplier route added

--- Routes/supplierRoute.js
+++ Routes/supplierRoute.js
@@ -4,32 +4,50 @@ const router = express.Router();
 var nodemailer = require('nodemailer');
 
 
-router.get('/',function(req,res){
-    res.json({message: 'welcome to supplier routes'});
+router.get('/', function (req, res) {
+    res.json({ message: 'welcome to supplier routes' });
 });
 
 router.route('/supplier')
-.get(function (req,res) {
-    suppliers.find(function(err,supplier){
-        if(err){
-            console.log(err);
-        }
-        res.json(supplier);
-    });
+    .get(function (req, res) {
+        suppliers.find(function (err, supplier) {
+            if (err) {
+                console.log(err);
+            }
+            res.json(supplier);
+        });
 
-});
+    });
 
 router.route('/supplier')
-.post(function(req, res) {
-    var supplier = new suppliers(req.body);      
-   
-    supplier.save(function(err) {
-        if (err)
-            res.send(err);
-        else
-            res.json({ message: 'Supplier added!' });
+    .post(function (req, res) {
+        var supplier = new suppliers(req.body);
+
+        supplier.save(function (err) {
+            if (err)
+                res.send(err);
+            else
+                res.json({ message: 'Supplier added!' });
+        });
+
     });
 
-});
-module.exports= router;
+//edit supplier
+router.route('/supplier/:id')
+    .put(function (req, res) {
+        console.log("supplier updating");
+        suppliers.findByIdAndUpdate(
+            {_id:req.params.id},
+            req.body
+        ).then(function () {
+            suppliers.findOne({_id: req.params.id}).then(function (response) {
+                res.send(response);
+            });
+
+        });
+
+    });
+
+
+module.exports = router;
 
